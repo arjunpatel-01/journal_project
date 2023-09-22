2b97f3152bfa74afe3bf23467ae3f34687e4b6cc
Arjun Patel <55220892+arjunpatel01@users.noreply.github.com>
2023-09-22 01:24:23 -0500

    update models

--- lib/models/journal.dart
+++ lib/models/journal.dart
@@ -2,19 +2,22 @@ class Journal {
   int id;
   String title;
   String mood;
-  String? description;
+  String description;
+  String timestamp;
 
   Journal(
       {required this.id,
       required this.title,
       required this.mood,
-      this.description});
+      required this.description,
+      required this.timestamp});
 
   factory Journal.from(dynamic doc) => Journal(
       id: doc["id"],
       title: doc["title"],
       mood: doc["mood"],
-      description: doc["entry"]);
+      description: doc["description"],
+      timestamp: doc['timestamp']);
 
   Map<String, dynamic> toJson() =>
       {'id': id, 'title': title, 'mood': mood, 'description': description};
--- lib/models/journal_dto.dart
+++ lib/models/journal_dto.dart
@@ -1,9 +1,10 @@
 class JournalDTO {
   String title;
   String mood;
-  String? description;
+  String description;
 
-  JournalDTO({required this.title, required this.mood, this.description});
+  JournalDTO(
+      {required this.title, required this.mood, required this.description});
 
   factory JournalDTO.from(dynamic doc) => JournalDTO(
       title: doc['title'], mood: doc["mood"], description: doc["description"]);
