9c9da8b5035a64127bfa28e9a7074349786f8cca
Arjun Patel <55220892+arjunpatel01@users.noreply.github.com>
2023-09-22 01:24:41 -0500

    fixed routing

--- lib/routes/route_config.dart
+++ lib/routes/route_config.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:journal_project/models/journal.dart';
 import 'package:journal_project/pages/create_update_form.dart';
 import 'package:journal_project/pages/home.dart';
 
@@ -10,10 +11,11 @@ class MyAppRouter {
       builder: (context, state) => const HomePage(),
     ),
     GoRoute(
-      name: 'createorupdate',
-      path: '/createorupdate',
-      builder: (context, state) =>
-          CreateOrUpdatePage(id: state.pathParameters['id']),
-    ),
+        name: 'createorupdate',
+        path: '/createorupdate',
+        builder: (context, state) {
+          Journal? journal = state.extra as Journal?;
+          return CreateOrUpdatePage(journal: journal);
+        }),
   ]);
 }
